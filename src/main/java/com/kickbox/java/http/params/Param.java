63e31002ca49cea68a437c30131d41cbc9e2ea3c
Sergii Zagriichuk <sergii.zagriichuk@gmail.com>
2015-09-08 11:40:22 +0300

    Encoded url parameters.

--- src/main/java/com/kickbox/java/http/params/Param.java
+++ src/main/java/com/kickbox/java/http/params/Param.java
@@ -1,24 +1,39 @@
 package com.kickbox.java.http.params;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 /**
  * Base abstraction of the Kickbox.io's parameters.
  *
  * @author szagriichuk.
  */
 public abstract class Param<T> {
+    private static final Logger LOG = LoggerFactory.getLogger(Param.class);
     T value;
-
     public Param(T param) {
         this.value = param;
     }
 
     @Override
     public String toString() {
-        return name() + "=" + value;
+        return name() + "=" + encode(String.valueOf(value));
     }
 
     public abstract String name();
     public T value(){
         return value;
+    }
+
+    private String encode(String data){
+        try {
+            return URLEncoder.encode(data, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            LOG.error("Cannot encode request url param.", e);
+        }
+        return "";
     }
 }
